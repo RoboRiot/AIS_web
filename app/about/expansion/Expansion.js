c582f01528987031e74b5bf4953ccd70f4adcb83
Igor <igor.a.sav@gmail.com>
2026-09-16 17:00:42 -0700

    Replace duplicate About page expansion photo

--- app/about/expansion/Expansion.js
+++ app/about/expansion/Expansion.js
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import styles from "../how_we_are/howWeAre.module.scss"
-import HowWEImage from "@/public/assets/images/who_we_are.jpg"
+import expansionImage from "@/public/assets/images/exterior-1.jpg"
 
 export default function Expansion() {
     return(
         <>
             <div className={`${styles.how_we_are} ${styles.how_expansion}`}>
                 <div className="flex items-center container">
-                    <figure data-aos="fade-left" data-aos-duration="1000"><Image src={HowWEImage} alt="expertsImage" /></figure>
+                    <figure data-aos="fade-left" data-aos-duration="1000"><Image src={expansionImage} alt="Mobile MRI trailer exterior with access stairs" /></figure>
                     <section data-aos="fade-right" data-aos-duration="1000">
                         <h2>OUR <span> EXPANSION</span></h2>
                         <p>From our beginnings in Northern California, we have rapidly expanded to meet the growing demand for our services across the country. Our reputation for excellence has spread quickly, and we continue to grow, driven by the high quality of our work. Our clients recognize the value we bring, and we are proud to be their first choice for imaging services. We are committed to maintaining our high standards as we expand, ensuring every client receives the best service possible.</p>
